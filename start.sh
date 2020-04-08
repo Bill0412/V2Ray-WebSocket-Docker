de6ce2b13da3152f0a836b0a5e65085b9e01c749
mack-a <jerrymattmeng@gmail.com>
2020-04-08 17:19:40 +0800

    feat(脚本): update

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 sed -i "s/mainPath/$mainPath/g" `grep 'mainPath' -rl /etc/nginx/nginx.conf`
 sed -i "s/mainPath/$mainPath/g" `grep 'mainPath' -rl /usr/src/v2ray/config_ws_tls.json`
 
-/usr/src/v2ray/v2ray -config /usr/src/v2ray/config_ws_tls.json &
+# /usr/src/v2ray/v2ray -config /usr/src/v2ray/config_ws_tls.json &
 # /usr/bin/node /usr/src/node/server.js &
 # /usr/bin/crontab /usr/src/node/traffic.cron
 # /usr/sbin/crond
